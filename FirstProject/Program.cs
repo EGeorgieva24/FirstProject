be439c5d7a28bdead27cc7c625d3b7ffbf6563fe
e.georgieva24 <e.georgieva24@LAB108PC13>
2023-09-12 14:58:41 +0300

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("What is your height?");
+int input = Convert.ToInt32(Console.ReadLine());
+Check(input);
+
+  void Check (int a) 
+    { 
+    if (input<=147)
+{
+     Console.WriteLine("You are a dwarf.");
+}
+    else if (input>147 && input<180)
+{
+     Console.WriteLine("You are a normal height.");
+}
+    else
+{
+     Console.WriteLine("You are too tall.");
+}
+}
+
